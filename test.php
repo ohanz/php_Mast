2d178a9be76ae73df5c46c436c1a135d30a595a6
ohanz <chiaguguadaniel@gmail.com>
2024-09-04 02:07:36 +0100

    second comm

--- test.php
+++ test.php
@@ -1,5 +1,21 @@
 <!-- a prpject to re-master php-->
 
 <?php
-echo "<h1> My Name is Ohanz </h2>";
+  $name = 'Ohanzo';
+  $ageMax = 34;
+ $ageLim = 25;
+ $myAge= 20;
+echo "<h1> My Name is $name</h2>". "<h2> and I am " .$myAge. " years old. </h2>" ;
+
+  if($ageMax > $myAge && $myAge < $ageLim) {
+    echo "<i>Jahda's Reply: </i>"."<br /><h3>You're not Eligible.</h3>";
+}
+elseif($ageMax < $myAge && $myAge > $ageLim ){
+  echo "<i>Jahda's Reply: </i>"."<br /><h3>You're not Eligible.</h3>";
+}
+elseif($ageMax > $myAge && $myAge < $ageLim){
+  echo "<i>Jahda's Reply: </i>"."<br /><h3>You're Eligible.</h3>";
+}
+
+
 ?>
