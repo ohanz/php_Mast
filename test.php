8b292d1dd197bd3e86931b3a3b888e8eff2bfe93
ohanz <chiaguguadaniel@gmail.com>
2024-09-04 03:31:54 +0100

    add basic form

--- test.php
+++ test.php
@@ -5,7 +5,8 @@
   $ageMax = 34;
  $ageLim = 25;
  $myAge= 20;
-echo "<h1> My Name is $name</h2>". "<h2> and I am " .$myAge. " years old. </h2>" ;
+ echo "<u>Welcome ", "to ", "Php Master ", "Relearn ", "Test Base.</u>";
+echo "<br/><h1> My Name is $name</h2>". "<h2> and I am " .$myAge. " years old. </h2>" ;
 
   if($ageMax > $myAge && $myAge < $ageLim) {
     echo "<i>Jahda's Reply: </i>"."<br /><h3>You're not Eligible.</h3>";
@@ -16,6 +17,8 @@ elseif($ageMax < $myAge && $myAge > $ageLim ){
 elseif($ageMax > $myAge && $myAge < $ageLim){
   echo "<i>Jahda's Reply: </i>"."<br /><h3>You're Eligible.</h3>";
 }
+
+echo "<br/><br/>";
 
 
 ?>
---
+++ wel_post.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+<h1>Hye there!</h1> <br /><h3>Welcome <?php echo $_POST["name"] . "."; ?></h3>
+<h3>Your email address is: <?php echo $_POST["email"]; ?></h3>
+
+</body>
+</html>
