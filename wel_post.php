758bddbfb13810ed155f756f41325557e6590d96
ohanz <chiaguguadaniel@gmail.com>
2024-09-05 20:56:34 +0100

    added new input

--- wel_post.php
+++ wel_post.php
@@ -3,6 +3,7 @@
 
 <h1>Hye there!</h1> <br /><h3>Welcome <?php echo $_POST["name"] . "."; ?></h3>
 <h3>Your email address is: <?php echo $_POST["email"]; ?></h3>
+<h3>Your age: <?php echo $_POST["ageRange"]; ?> is Eligible.</h3>
 
 </body>
 </html>
